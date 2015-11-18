f818a5dc5d1ca79cf6e44824a12b96cf5bded5c5
Alexey Gorshkov <animus@wayround.org>
2015-11-18 12:29:10 +0300

    ic

---
+++ wayround_org/mail/server/imap.py
@@ -0,0 +1,22 @@
+
+
+import wayround_org.utils.socket
+
+
+def read_next_input_block(sock, stop_flag, limit=20 * (1024**2)):
+    # TODO wayround_org.utils.socket....
+    return
+
+
+class SocketListener:
+    """
+    Class for listening socket connections, working with
+    client command  identifiers and routing to controller
+    """
+
+    def __init__(self, controller):
+        self._controller = controller
+        return
+
+    def socket_server_target(self):
+        return
